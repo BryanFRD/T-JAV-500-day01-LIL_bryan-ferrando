22ace8a8badb9ab19b7b4517c6b9a2282bba554e
Bryan <bryan.ferrando@epitech.eu>
2023-10-23 10:19:13 +0100

    Ex02

--- ex_02/Ex02.java
+++ ex_02/Ex02.java
@@ -2,6 +2,10 @@ package ex_02;
 
 public class Ex02 {
 
+    public static void main(String[] args) {
+        System.out.println(getAngryDog(3));
+    }
+
     public static String getAngryDog(int nbr){
         String dog = "";
         for(int i = 0; i < nbr; i++){
