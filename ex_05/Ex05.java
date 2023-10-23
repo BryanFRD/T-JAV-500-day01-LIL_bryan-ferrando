f779f936c108b2c0ce3b84950637ef4b2e01adfc
Bryan <bryan.ferrando@epitech.eu>
2023-10-23 09:41:13 +0100

    Ex05

---
+++ ex_05/Ex05.java
@@ -0,0 +1,12 @@
+package ex_05;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ex05 {
+
+    public static ArrayList<String> myGetArgs(String... var){
+        return new ArrayList<String>(Arrays.asList(var));
+    }
+
+}
